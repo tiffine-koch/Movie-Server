cf16be69e104545b3ea4713c4208ef748872d484
Apple <tiffine.koch@gmail.com>
2016-03-01 08:52:36 -0800

    push for homework

--- main.js
+++ main.js
@@ -1,88 +1,49 @@
 'use strict';
 
-var movieList = [], text;
-
 $(function() {
-  // $('#catDropDown').on('click', findCat);
   $('#submit').click(sendRequest);
 });
+// var movieList = [], text, $card;
 
 function sendRequest() {
   var title = $('#movieInput').val().trim();
   var type = $('#catDropDown').text();
   var year = $('#yearInput').val();
+  $('#alert').empty();
+  $('#movieDisplay').empty();
 
   $.ajax({
 
-    url: "http://www.omdbapi.com/?s=" + title + '&type=' + type + '&yr=' + year + "&r=json",
+    url: "http://www.omdbapi.com/?s=" + title + "&type=" + type + "&yr=" + year + "&r=json",
+
     type: "GET",
     success: function(data) {
-      console.log('data', data);
-      movieList = data;
-      createList();
-      console.log('movieList', movieList);
-    },
-    error: function(err) {
-      console.error('error', err);
-      var $message = $('<p>').text('You are drunk. Less Chill and more Netflix.'); //create bootstrap alert
-      $('#alert').append($message);
-    }
+      var movieList = data.Search;
+
+      if(data.Response === 'True') {
+        var $movie = movieList.map(function(movie) {
+          // var $title = $('<div>').text(movie.Title).addClass('title');
+          // var $year = $('<div>').text(movie.Year).addClass('year');
+          var $image= '<img class="col-xs-4"' + 'src=' + movie.Poster + '>'
+          var $imdb = 'http://www.imdb.com/title/' + movie.imdbID;
+          // $('#movieDisplay').append($title);
+          // $('#movieDisplay').append($year);
+          $('#movieDisplay').append('<a href=' + $imdb + '>' + $image + '</a>').addClass('card');
+        });
+
+      } else if(data.Response === 'False') {
+          var $message = $('<div>').text('You are drunk. Less Chill and more Netflix. Try again').addClass('message'); //create bootstrap alert
+          $('#alert').append($message);
+          console.error('error', err);
+        }
+      }
   });
 }
 
 function createList() {
-  // // console.log('year:', year);
-  //
-  // var title = $('#movieInput').val().trim();
-  // console.log("title", title);
-  // // var title = $('movieInput').val().toLowerCase().trim();
-  //
-  // var filtered = movieList.filter(function(data) {
-  //   return data.Title == title;
-  // });
-  //
-  // var movie = filtered[0];
-  console.log('create list');
-  // if(data.Reponse === 'True') {
-  //   console.log('inside if');
-  //   for(var i = 0; i < movieList.length; i++) {
-  //     var $card = makeMovieCard(data);
-  //     $('#movieDisplay').append($card);
-  //   }
-  // }
-  // if(data.Reponse === 'True') {
-  //   $.get(data.Title, function(data) {
-  //     for(var i = 0; i < movieList.length; i++) {
-  //       var $card = makeMovieCard(data);
-  //       $('#movieDisplay').append($card);
-  //       }
-  //     });
-  //   } else {
-  //     var $message = $('<p>').text('You are drunk. Less Chill and more Netflix.'); //create bootstrap alert
-  //     $('#alert').append($message);
-  //   }
-  }
-
-function makeMovieCard(data) {
-
-  var $card = $('#template').clone();
-  $card.removeAttr('id');
-  console.log('hey everyone', movieList);
-
-  $card.find('.title').text(data.Title);
-  $card.find('.image').attr('src', data.Poster);
-  $card.find('.link').attr('href', 'https://www.imdb.com/title/' + data.imdbID);
-  $card.find('.year').text(data.Year);
-  $card.find('.rated').text(data.Rated);
-
-  return $card;
-};
-
-// function findCat(event) {
-//   console.log('inside findCat');
-//   event.preventDefault();
-//   var text = $(this).text();
-//
-//   $('#catEntry').text(text).append('<span class="caret"></span>');
-//   console.log(text);
-// }
+  console.log('create list function');
+  var $movie = data.map(function(movie){
+      makeMovieCard(data);
+    });
+  $('#movieDisplay').append($movie);
+}
